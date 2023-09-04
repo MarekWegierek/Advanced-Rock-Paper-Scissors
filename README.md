bbfb77273c2613b41eff01ba3bbe31b440da1687
Marek Węgierek <mpwegierek@gmail.com>
2023-09-04 22:28:47 +0200

    Added full current figure background, changed colors in css into variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Advanced Rock Paper Scissors Project in vanilla JavaScript 
+# Advanced Rock Paper Scissors Project in vanilla JavaScript
 
 ## This is a base model project, created with intention to translate it in the future to React and React Native to mobile app
 
@@ -11,34 +11,34 @@ I've based my idea and take icons from this image I've found on the internet:
 <img src='images/8496.jpg' height='500px' width='500px'>
 made by <a href='https://twitter.com/hamsterdunce' target='_blank'>David C. Lovelace</a>
 
- My roadmap to this project looks like that:
+My roadmap to this project looks like that:
 
-* v0.1 functional desktop version of app (we are here)
-* v0.2 mobile browser version 
-* v0.3 option of playing with computer
-* v0.4 3 different modes of game to choose from
-* v0.5 Day/Night Mode interface
-* v0.6 Player vs Player Mode
-* v0.7 Add Difficulty Levels for playing with computer
-* v0.8 Add sound effects 
-* v0.9 Fix any problems, that might happend in the meantime
-* v1.0 Final version 
+- v0.1 functional desktop version of app (we are here)
+- v0.2 mobile browser version
+- v0.3 option of playing with computer
+- v0.4 3 different modes of game to choose from
+- v0.5 Day/Night Mode interface
+- v0.6 Player vs Player Mode
+- v0.7 Add Difficulty Levels for playing with computer
+- v0.8 Add sound effects
+- v0.9 Fix any problems, that might happend in the meantime
+- v1.0 Final version
 
 ## How to use it?
 
 1. Clone this project
 2. Open index.html using your browser
-3. Click on the icon/card of your chosing. It will show up what option you beat with your icon (green) and which one will beat you (red). 
+3. Click on the icon/card of your chosing. It will show up what option you beat with your icon (green) and which one will beat you (red).
 4. Context menu on the right will help you if you are confused by UI or are colorblind
 
-## How to actually play that game?
+## How to actually play that game (when it will be playable)?
 
 1. Click the card on the wheel
 2. In the window below you should see the icon of your choosing
 3. Confirm your choice by cliking 'Done'
 4. 1 If you playing against computer, the computer will make a turn
-4. 2 If you playing against player, your conformed option should hide. Now it's time for the other player to make a turn
-5. Whatever you play against computer, or another player, after both choices are made, the match has begun. 
-6. <b>For each win player get 1 points, and for each lost match, you lose 1</b>
-7. Both of players starts with 0 points. Player who get 3 points first will win.
-8. None of players can have less than 0 points.
+5. 2 If you playing against player, your conformed option should hide. Now it's time for the other player to make a turn
+6. Whatever you play against computer, or another player, after both choices are made, the match has begun.
+7. <b>For each win player get 1 points, and for each lost match, you lose 1</b>
+8. Both of players starts with 0 points. Player who get 3 points first will win.
+9. None of players can have less than 0 points.
--- script.js
+++ script.js
@@ -1,89 +1,100 @@
-import cardList from './cards.js';
-const possibleChoices = document.querySelectorAll('.card-button');
-const userChoiceDisplay = document.getElementById('current-card');
-const currentCardBeats = document.getElementById('current-card-beats');
-const currentCardBeatenBy = document.getElementById('current-card-beaten-by');
+import cardList from './cards.js'
+const possibleChoices = document.querySelectorAll('.card-button')
+const userChoiceDisplay = document.getElementById('current-card')
+const currentCardBeats = document.getElementById('current-card-beats')
+const currentCardBeatenBy = document.getElementById('current-card-beaten-by')
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
-    const currTargetClassList = e.originalTarget.offsetParent.classList;
-    let figure = '';
-    let beatsArray = [];
-    let beatenByArray = [];
-    let beatsArrayBtnName = [];
-    let beatenByArrayBtnName = [];
-
+    const currTargetClassList = e.originalTarget.offsetParent.classList
+    let figure = ''
+    let beatsArray = []
+    let beatenByArray = []
+    let beatsArrayBtnName = []
+    let beatenByArrayBtnName = []
+    console.log(currTargetClassList)
     /* Clean html list on every click */
-    currentCardBeats.replaceChildren();
-    currentCardBeatenBy.replaceChildren();
+    currentCardBeats.replaceChildren()
+    currentCardBeatenBy.replaceChildren()
 
     /* Assign values to varaibles above  */
     cardList.forEach((card) => {
       if (card.btnName == currTargetClassList[1]) {
-        let { name, beats, beatenBy } = card;
-        figure = name;
-        beatsArray = beats;
-        beatenByArray = beatenBy;
+        let { name, beats, beatenBy } = card
+        figure = name
+        beatsArray = beats
+        beatenByArray = beatenBy
       }
-    });
+    })
 
     /*Turning variables into html elements */
 
     function makeList(array, htmlElement) {
       array.forEach((element) => {
-        const newElement = document.createElement('li');
-        let newContent = document.createTextNode(element);
-        newElement.appendChild(newContent);
-        htmlElement.appendChild(newElement);
-      });
+        const newElement = document.createElement('li')
+        let newContent = document.createTextNode(element)
+        newElement.appendChild(newContent)
+        htmlElement.appendChild(newElement)
+      })
     }
 
     /* Pushing made above elements into html */
-    userChoiceDisplay.innerHTML = figure;
-    makeList(beatsArray, currentCardBeats);
-    makeList(beatenByArray, currentCardBeatenBy);
+    userChoiceDisplay.innerHTML = figure
+    makeList(beatsArray, currentCardBeats)
+    makeList(beatenByArray, currentCardBeatenBy)
 
     /*Create array for all names of the buttons from beatsArray i beatenByArray*/
     /* SHOULD BE SHORTEN UP */
     beatsArray.forEach((cardName) => {
       cardList.forEach((card) => {
         if (cardName == card.name) {
-          beatsArrayBtnName.push(card.btnName);
+          beatsArrayBtnName.push(card.btnName)
         }
-      });
-    });
+      })
+    })
 
     beatenByArray.forEach((cardName) => {
       cardList.forEach((card) => {
         if (cardName == card.name) {
-          beatenByArrayBtnName.push(card.btnName);
+          beatenByArrayBtnName.push(card.btnName)
         }
-      });
-    });
+      })
+    })
+
+    /* Add/remove background to current figure */
+    possibleChoices.forEach((icon) => {
+      if (Array.from(icon.classList).includes('figure')) {
+        icon.classList.remove('figure')
+      }
+    })
+    if (Array.from(currTargetClassList).includes('card-button')) {
+      currTargetClassList.add('figure')
+    }
+
     /* SHOULD BE SHORTEN UP */
     /* Check all cards that beats (green rim) */
 
     possibleChoices.forEach((icon) => {
       if (Array.from(icon.classList).includes('beats')) {
-        icon.classList.remove('beats');
+        icon.classList.remove('beats')
       }
-    });
+    })
     possibleChoices.forEach((icon) => {
       if (beatsArrayBtnName.includes(icon.classList[1])) {
-        icon.classList.add('beats');
+        icon.classList.add('beats')
       }
-    });
+    })
     /* Check all cards that are beaten up (red rim) */
 
     possibleChoices.forEach((icon) => {
       if (Array.from(icon.classList).includes('beaten-by')) {
-        icon.classList.remove('beaten-by');
+        icon.classList.remove('beaten-by')
       }
-    });
+    })
     possibleChoices.forEach((icon) => {
       if (beatenByArrayBtnName.includes(icon.classList[1])) {
-        icon.classList.add('beaten-by');
+        icon.classList.add('beaten-by')
       }
-    });
+    })
   })
-);
+)
